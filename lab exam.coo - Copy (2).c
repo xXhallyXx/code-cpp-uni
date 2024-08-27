b820e010a70da2dfba435183225234e63e0b6c1c
xXhallyXx <133434454+xXhallyXx@users.noreply.github.com>
2024-08-27 15:37:28 +0600

    Add files via upload

---
+++ lab exam.coo - Copy (2).c
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+int main ()
+{
+    char a[100];
+    cin>>a;
+    int len=strlen(a);
+
+    return 0;
+}
---
+++ matrix 2nd change.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    int n,m;
+    cin>>n>>m;
+    int a1[n][m];
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<m; j++)
+        {
+            cin>>a1[i][j];
+
+        }
+    }
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<m; j++)
+        {
+            cout<<a1[i][j]<<" ";
+
+        }
+        cout<<endl;
+    }
+    for(int j=1;j>=0;j--)
+    {
+        for(int  i=1;i>=0;i--)
+        {
+            cout<<a1[j][i]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+
